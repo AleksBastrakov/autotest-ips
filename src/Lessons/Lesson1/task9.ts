fc07b2e056b80e2414dda0efdf4b09f5e7f90038
AleksBastrakov <aleksbastrakov@mail.ru>
2023-03-02 21:45:14 +0300

    Update task9.ts

--- src/Lessons/Lesson1/task9.ts
+++ src/Lessons/Lesson1/task9.ts
@@ -1,9 +1,9 @@
 let anon = function() {
-    let n:number = 10
+    let n: number = 10
     if (n >= 18)
         console.log('Страница доступна')
     else
         console.log('Страница не доступна')
 }
 
-anon();
+anon()
